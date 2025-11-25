9d6b4ae1a2811eb7effe62727b3f8afcb66912d6
mikeill <mike@mzoo.org>
2025-11-24 18:05:27 -0600

    Working python version sliding window longest substring.

---
+++ algorithms/longest-string.py
@@ -0,0 +1,32 @@
+'''
+Use a hash map to store the last index of each character.
+use 'left' and 'right' index indicators to evaluate contents of a siding window
+loop through the string by right index and if char is not present in the map, add it at the index of ch
+increment right index
+potentially incremenet longest
+if char is present, set left to the index of the char in the map + 1
+set the index of the char in the map to the current index
+increment right index
+return longest
+'''
+
+def lols(s: str) -> int:
+    n = len(s)
+    charMap = {}
+    longest = 0
+    left = 0
+    for right in range(len(s)):
+        if s[right] not in charMap or charMap[s[right]] < left:
+            charMap[s[right]] = right
+            longest = max(longest, right - left + 1)
+        else:
+            left = charMap[s[right]] + 1
+            charMap[s[right]] = right
+        #print(charMap)
+    return longest
+
+print(lols("aaclaxlphabbb")) # 6
+print(lols("aaclaxlphabygbb")) # 8
+print(lols("")) # 0
+print(lols("aaclaxlphabyglphabygaaabb")) # 8
+print(lols("aaclaxlphabayglphabygaaabb")) # 7
