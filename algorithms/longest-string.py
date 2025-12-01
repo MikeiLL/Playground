abac36a3e85a7c8c89fec05a1d121f5cc84c9352
mikeill <mike@mzoo.org>
2025-12-01 17:55:11 -0600

    Python longest substring with notes.

--- algorithms/longest-string.py
+++ algorithms/longest-string.py
@@ -3,36 +3,35 @@ Use a hash map to store the last index of each character.
 use 'left' and 'right' index indicators to evaluate contents of a siding window
 loop through the string by right index
   AND within the loop
-    IF char IS NOT present in the map,
-      add it at the index of ch
-    increment right index
+    IF (
+          char IS NOT present in the map,
+          OR left has already moved passed its mapped value
+        )
+      set/add it at the index of ch (right)
+    increment right index by (one more than the difference between right and left)
     IF window size > longest, incremenet longest
-    IF char IS present
-      set left to the index of the char in the map + 1
+    ELSE (char IS present)
+      set left to the index of the char->value in the map + 1
     set the index of the char in the map to the current index
     increment right index
 return longest
 '''
 
 def lols(s: str) -> int:
-    n = len(s)
     charMap = {}
     longest = 0
     left = 0
     for right in range(len(s)):
         if s[right] not in charMap or charMap[s[right]] < left:
-            if ((charMap.get(s[right]) or 1000) < left):
-                print("\033[0;32mGot one for %s, map: %s left: %s right: %s.\033[0;0m" % (s[right], charMap.get(s[right]), left, right))
             charMap[s[right]] = right
             longest = max(longest, right - left + 1)
         else:
             left = charMap[s[right]] + 1
             charMap[s[right]] = right
-        print(charMap, left)
     return longest
 
-#print(lols("aaclaxlphabbb")) # 6
-#print(lols("aaclaxlphabygbb")) # 8
-#print(lols("")) # 0
-print(lols("pphapahabyzygaaabb")) # 6
-#print(lols("aaclaxlphabayglphabygaaabb")) # 7
+print(lols("aaclaxlphabbb") == 6) # claxlp
+print(lols("aaclaxlphabygbb") == 8) # xlphabyg
+print(lols("") == 0)
+print(lols("pphapahabyzygaaabb") == 5) # habyz
+print(lols("aaclaxlphabayglphabygaaabb") == 7) # bayglph
