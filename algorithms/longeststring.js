c52c63729d967cd4a586709be9be8aaa46cff568
mikeill <mike@mzoo.org>
2025-12-02 19:58:18 -0600

    Correct in js with insightful logging.

--- algorithms/longeststring.js
+++ algorithms/longeststring.js
@@ -1,57 +1,36 @@
 /* Return length of longest substr
  - split str into array
- - seen dictionary
- - start
- - cur
- 1. cur and start at 0
- 2. while cur < arr.length
-  add cur to seen with value of position
+
 */
+
 function len_longestsubstr(str) {
-  if (str.length === 0) return "";
-  let start = 0;
-  let cur = 0;
-  const arr = str.split("");
-  let longest = [];
-  let seen = {};
-  while (cur < arr.length) {
-    curVal = arr[cur];
-    if (curVal in seen) {
-      console.log("seen ", curVal);
-      start++;
-    } else {
-      console.log("not seen ", curVal);
-    }
-    seen[curVal] = cur;
-    cur++;
-    if (longest.length < (cur+1 - start)) {
-      //console.log("reset longest", longest, start, cur);
-      longest = arr.slice(start, cur);
-      console.log("new longest", longest, start, cur);
-      /*
-      alphab
-       |  |
-      */
-    } else longest.push(curVal);
-    /* console.log({
+  let left = 0;
+  const seen = {};
+  let longest = 0;
+  for (const right of Array(str.length).keys()) {
+    console.log({
       "string": str,
-      "longest": longest.join(""),
-      "longest len": longest.length,
-      "cur - start": cur - start,
-      "LETTER": curVal, "start": start, "cur": cur, "seen": seen
-    }); */
-  }
-  return longest.join('');
+      "substring": str.substring(left, right),
+      "longest": longest,
+      "diff": right - left + 1,
+      "left": left,
+      "right": right,
+      "char": str[right],
+      "lastseen": seen[str[right]],
+    });
+    if (!(str[right] in seen) || seen[str[right]] < left) {
+      longest = Math.max(longest, right - left + 1);
+    } else {
+      left = seen[str[right]] + 1;
+    }
+    seen[str[right]] = right;
+    }
+  return longest;
 }
 
-// console.log(len_longestsubstr("aabcdefghhh")); // bcdefgh
-// console.log(len_longestsubstr("")); // 0
-//console.log(len_longestsubstr("alphabetical")); // 8
-console.log(len_longestsubstr("alphab")); // lphab
-// console.log(len_longestsubstr("aaaaaaaa")); // 1
-// console.log(len_longestsubstr("aaababcabcdabcdefagah")); // 6
-
-// start arr[start] == a
-// cur arr[1] == a
-// "alphabetical"
-//   |  |
+console.log(len_longestsubstr("aabcdefghhh"), 8); // bcdefgh
+//console.log(len_longestsubstr(""), 0); // 0
+//console.log(len_longestsubstr("alphabetical"), 8); // 8
+//console.log(len_longestsubstr("alphab"), 5); // lphab
+//console.log(len_longestsubstr("aaaaaaaa"), 1); // 1
+//console.log(len_longestsubstr("aaababcabcdabcdefagah"), 7); // 6
