b635916f8fb2d244efa899b7e5e52fe534b36bbc
mikeill <mike@mzoo.org>
2026-04-04 20:33:32 -0500

    Perhaps 167 isn't the space character.

--- NCL/numericCodeBreak.py
+++ NCL/numericCodeBreak.py
@@ -5,6 +5,7 @@ from collections import Counter
 code = [254, 232, 213, 227, 214, 222, 210, 167, 222, 232, 235, 235, 167, 209, 236, 225, 214, 214, 211, 167, 237, 214, 209, 167, 224, 213, 167, 220, 215, 227, 224, 211, 236, 167, 232, 213, 167, 145, 145, 167, 212, 232, 213, 220, 211, 236, 210]
 ct = Counter(code)
 from pprint import pprint
+word2 = [222, 232, 235, 235,]
 pprint(ct)
 '''
 >>> min(code)
@@ -51,9 +52,32 @@ Counter({167: 8,
          212: 1})
 '''
 
+space = 32
+upper_case_start_at = 65
+lower_case_end_at = 122
+# 122 - 32 = 90 so
+# range btwn chars is 90
+# 254 - 145 range between numbers in code: 109
+# upper case Z = 90
+'''
+The most common letters in English, from most to least frequent, are E, T, A, O, I, N, S, H, R, and D. The letter E appears in about 12.7% of words, making it the most frequently used letter.
+'''
 
+import re
+# may need to install wordlist with apt
+file = open("/usr/share/dict/words", "r")
+words = re.sub("[^\w]", " ",  file.read()).split()
+file.close()
+    
+def is_word(word):
+    return word.lower() in words
 
-msg = ""
-for x in code:
-    msg += chr(x - 135)
-print(msg+"\n")
+for n in range(50, 159):
+    msg = ""
+    for x in word2:
+        if x-n < 32: print("below 32")
+        else: msg += chr(x - n)
+    if is_word(msg):
+        print("Message: " + msg+"\n")
+    else: print("-")
+
